e131e82cff79ed5bda351561097551d8f4ca805e
karakuscem <karakuscemenes@gmail.com>
2023-08-22 23:18:29 +0300

    project created

---
+++ src/test/java/com/tourismcorp/healthtourism/HealthTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourismcorp.healthtourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthTourismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
