bca03b04492584342b5d71f0bd395b5618c5d506
karakuscem <karakuscemenes@gmail.com>
2023-08-27 19:39:44 +0300

    todos added

--- src/main/java/com/tourismcorp/healthtourism/HealthTourismApplication.java
+++ src/main/java/com/tourismcorp/healthtourism/HealthTourismApplication.java
@@ -10,9 +10,18 @@ public class HealthTourismApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HealthTourismApplication.class, args);
-
-        // Null check for requestDTO mapper
-        // Delete function for all child classes
     }
+
+    /* TODO:
+        * 1. Aynı banka hesabı birden fazla user veya şirket ile ilişkilendirilememeli.
+        * 2. Uçuş konusunda seat kontrolü yapılmalı.
+        * 3. Uçuş konusunda uçuşun kalkış ve varış noktaları aynı olmamalı.
+        * 4. Uçuş, Randevu ve Otel için geçmiş tarih seçilmemeli.
+        * 5. Otel müsaitlik kontrolü yapılmalı.
+        * 6. Doktor müsaitlik kontrolü yapılmalı.
+        * 7. Randevu için doktor ve hasta aynı olmamalı.
+        * 8. Basit ödeme sistemi eklenebilir.
+        * 9. Uygun exceptionlar eklenebilir.
+     */
 
 }
