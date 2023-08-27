627beec4dae0e8a1ec55192063ddabc62b002f6c
karakuscem <karakuscemenes@gmail.com>
2023-08-27 19:39:34 +0300

    basic generator added

--- src/main/java/com/tourismcorp/healthtourism/util/FlightIdGenerator.java
+++ src/main/java/com/tourismcorp/healthtourism/util/FlightIdGenerator.java
@@ -1,2 +1,15 @@
-package com.tourismcorp.healthtourism.util;public class FlightIdGenerator {
+package com.tourismcorp.healthtourism.util;
+
+public class FlightIdGenerator{
+
+    private static final long INITIAL_VALUE = 1000;
+    private static final long INCREMENT_SIZE = 100;
+    private static long currentValue = INITIAL_VALUE;
+
+
+    public static String getFlightNumber() {
+        currentValue += INCREMENT_SIZE;
+        return "TK" + "-" + "-" + currentValue + "-" + "-" + Math.round(Math.random() * 1000000);
+    }
+
 }
